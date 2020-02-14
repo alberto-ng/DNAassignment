24560138d8307d3310377174afe5713d4eba825e
alberto-ng <ang@chapman.edu>
2020-02-14 21:05:30 +0000

    reading each line and getting sum of char in file

--- assignment1.cpp
+++ assignment1.cpp
@@ -7,16 +7,24 @@ using namespace std;
 int main(int argc, char** argv){
     if (argc > 1){
         ifstream testFile;
-        char letter1;
+        string currLine;
+        char currLetter;
+        int sum = 0;
 
         testFile.open(argv[1]);
 
 
-        testFile >> letter1;
+        testFile >> currLine;
         while (!testFile.fail()){
-            cout << letter1 << endl;
-            testFile >> letter1;
+            cout << currLine << endl;
+            for (int i = 0; i < currLine.length(); ++i){
+                sum += 1;
+                currLetter = currLine[i];
+            }
+            testFile >> currLine;
         }
+
+        cout << sum << endl;
 
         testFile.close();
     }
