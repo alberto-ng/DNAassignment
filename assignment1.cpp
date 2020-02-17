8807ab8c4f75da2923211b49516cac7dad0311dc
alberto-ng <ang@chapman.edu>
2020-02-17 01:20:31 +0000

    finding probability of A,C,T,G

--- assignment1.cpp
+++ assignment1.cpp
@@ -14,11 +14,14 @@ int main(int argc, char** argv){
         int sumChar = 0;
         int sumLine = 0;
         int charInLine = 0;
+        int countA = 0;
+        int countG = 0;
+        int countC = 0;
+        int countT = 0;
         double differenceSqr = 0.0;
         double sumForVar = 0.0;
-        double variance = 0.0;
-        double stdDev = 0.0;
-        double mean = 0.0;
+        double mean, variance, stdDev;
+        double probA, probG, probC, probT;
 
         testFile.open(argv[1]);
         statFile.open(argv[1]);
@@ -28,6 +31,19 @@ int main(int argc, char** argv){
             sumLine += 1;
             cout << currLine << endl;
             for (int i = 0; i < currLine.length(); ++i){
+                if (currLine[i] == 'A' || currLine[i] == 'a'){
+                    countA += 1;
+                }
+                else if (currLine[i] == 'T' || currLine[i] == 't'){
+                    countT += 1;
+                }
+                else if (currLine[i] == 'G' || currLine[i] == 'g'){
+                    countG += 1;
+                }
+                else if (currLine[i] == 'C' || currLine[i] == 'c'){
+                    countC += 1;
+                }
+
                 sumChar += 1;
                 currLetter = currLine[i];
             }
@@ -36,6 +52,17 @@ int main(int argc, char** argv){
 
         cout << "\nnumber of char(sum): " << sumChar << endl;
         cout << "number of line: " << sumLine << endl;
+
+        probA = (((double)countA)/sumChar) * 100;
+        probG = (((double)countG)/sumChar) * 100;
+        probC = (((double)countC)/sumChar) * 100;
+        probT = (((double)countT)/sumChar) * 100;
+
+        cout << "\nProbability of A: " << probA << "%" << endl;
+        cout << "Probability of C: " << probC << "%" << endl;
+        cout << "Probability of T: " << probT << "%" << endl;
+        cout << "Probability of G: " << probG << "%" << endl;
+
 
         mean = ((double)sumChar)/sumLine;
         cout << "\nMean (sum/lines): " << mean << endl;
